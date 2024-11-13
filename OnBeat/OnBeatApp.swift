26f45601bbd4f8a640955e077e88e433247f90de
Adrián Alejandro Ramírez Cruz <adrian.rmz.587@gmail.com>
2024-11-13 16:25:11 +0100

    Initial Commit

---
+++ OnBeat/OnBeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBeatApp.swift
+//  OnBeat
+//
+//  Created by Adrián Alejandro Ramírez Cruz on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct OnBeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
